6e0af022f9e4e66e9754a3ee2f7b00da1e022d4a
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-02-26 15:39:16 +0500

    inheritance understood

--- recap.js
+++ recap.js
@@ -1,37 +1,57 @@
+// Inheritance
+// ! constructor is not need
 class Animals{
-    constructor(name, sound){
-        this.name = name;
-        this.sound = sound;
-    }
-    defineSounds(){
-        console.log(`${this.name} barks like ${this.sound}`)
-    }
+    constructor(name,food,time){
+        this.name=name;
+        this.food=food;
+        this.time=time;
+    } // even though we remove this it will not affect the final result
+    alive = true;
+    diet(){
+        console.log(`${this.name} eats ${this.food}`)
+    };
+    sleep(){
+        console.log(`${this.name} sleeps ${this.time} hours`)
+    };
 }
-const dog = new Animals('Dog', 'woof-woof')
-dog.defineSounds()
+class Rabbit extends Animals{
+    name= 'Rabbit';
+    food = 'carrots';
+    time = 7
+}
+class Fish extends Animals{
+    name= 'Fish';
+    food = 'seaweed';
+    time = 4
+}
+class Owl extends Animals{
+    name= 'Owl';
+    food = 'rodents';
+    time = 5
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const rabbit = new Rabbit();
+const fish = new Fish();
+const owl = new Owl();
+rabbit.diet();
+rabbit.sleep();
+fish.diet();
+fish.sleep();
+owl.diet();
+owl.sleep();
+console.log(owl.alive)
+// class Animals{
+//     constructor(name, sound){
+//         this.name = name;
+//         this.sound = sound;
+//     }
+//     defineSounds(){
+//         console.log(`${this.name} barks like ${this.sound}`)
+//     }
+// }
+// const dog = new Animals('Dog', 'woof-woof')
+// dog.defineSounds()
+//////////////////////////////
 // class Products{
 //    constructor(name,price){ //this is a method
 //        this.name=name; //defining properties
